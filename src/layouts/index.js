61e3c418463b33b9da2637e6b461427e85e325a6
vlbee <32312712+vlbee@users.noreply.github.com>
2018-06-30 17:25:45 +0100

    set up boilerplate

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Link from 'gatsby-link';
 import Helmet from 'react-helmet'
+import styled from 'styled-components';
 
 import Header from '../components/header'
 import './index.css'
@@ -15,14 +17,7 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,48 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+export default ({ data }) => {
+  console.log(data);
+  return (
+    <div>
+      <h1>
+        Amazing Pandas Eating Things
+      </h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Link
+            to={node.fields.slug}
+          >
+            <h3>
+              {node.frontmatter.title}{" "}
+              <span>— {node.frontmatter.date}</span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default IndexPage
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
