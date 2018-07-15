498f4128d66c1d58c7a6f2892839ae96fd473bb8
vlbee <32312712+vlbee@users.noreply.github.com>
2018-07-15 15:16:21 +0100

    add titlecase utility function

--- src/utils/titleCase.test.js
+++ src/utils/titleCase.test.js
@@ -1,7 +1,7 @@
 import titleCase from './titleCase'
 
 test('Title Case works', () => {
-  const str = ` acute aNgle-glaucoma `
-  const result = `Acute Angle-Glaucoma`
+  const str = ` acute aNgle-glaucoma trauma    `
+  const result = `Acute Angle-Glaucoma Trauma`
   expect(titleCase(str)).toBe(result);
 });
