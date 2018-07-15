8c2d1afa83ac903ad00d3af5838eb62b3193819c
vlbee <32312712+vlbee@users.noreply.github.com>
2018-07-15 15:05:48 +0100

    write title case test

--- src/utils/titleCase.test.js
+++ src/utils/titleCase.test.js
@@ -1,7 +1,7 @@
 import titleCase from './titleCase'
 
 test('Title Case works', () => {
-  const str = ` acute aNgle glaucoma `
-  const result = `Acute Angle Glaucoma`
+  const str = ` acute aNgle-glaucoma `
+  const result = `Acute Angle-Glaucoma`
   expect(titleCase(str)).toBe(result);
 });
