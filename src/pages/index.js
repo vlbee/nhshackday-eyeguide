3f3e93a52558bea9e115898da9ad7029b118ad9f
vlbee <32312712+vlbee@users.noreply.github.com>
2018-08-16 16:24:05 +0100

    add netlify identity scripts for cms admin authenticaton

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,67 @@
 import React from 'react'
+import Script from 'react-load-script';
 import { Wrapper } from '../components/wrapper'
 import TableContents from "../components/tablecontents"
 
-export default ({ data }) => {
+export default class IndexPage extends React.Component {
+  handleScriptLoad() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
+          });
+        }
+      });
+    }
+    window.netlifyIdentity.init();
+  }
 
-  const categories = data.allMarkdownRemark.edges.map(({ node }) => node.frontmatter.category[0]).filter(function (value, index, self) { return self.indexOf(value) === index });
-  return (
-    <Wrapper>
-      <h1>Guidelines</h1>
-      <TableContents categories={categories} data={data} />
-    </Wrapper>
-  );
-};
+  render() {
+
+    const { data } = this.props;
+    const categories = data.allMarkdownRemark.edges.map(({ node }) => node.frontmatter.category[0]).filter(function (value, index, self) { return self.indexOf(value) === index });
+
+    return (
+
+      <div>
+
+        <Wrapper>
+          <h1>Guidelines</h1>
+          <TableContents categories={categories} data={data} />
+        </Wrapper>
+        <Script
+          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          onLoad={this.handleScriptLoad.bind(this)}
+        />
+      </div>
+    )
+
+  }
+
+}
+
+
+
+
 
 export const query = graphql`
 query IndexQuery {
-  allMarkdownRemark(sort: {order: ASC, fields: [frontmatter___category, frontmatter___title]}) {
-    edges {
-      node {
-        id
+          allMarkdownRemark(sort: {order: ASC, fields: [frontmatter___category, frontmatter___title]}) {
+          edges {
+        node {
+          id
         fileAbsolutePath
         frontmatter {
           title
           date
-          category
-        }
+        category
+      }
         fields {
           slug
+        }
         }
       }
     }
   }
-}
 `;
